a432bd31a8280353e76cabe3c2bdef197e3103c0
amitk1531 <amitk1531@gmail.com>
2020-09-12 10:23:30 +0530

    Fetching the number of items for each SKU.

--- src/PrEngine/PrEngineProgram.cs
+++ src/PrEngine/PrEngineProgram.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace PrEngine
@@ -23,7 +24,16 @@ namespace PrEngine
             // Creating Object.
             Cart sku = new Cart();
 
-            Console.WriteLine(sku.unitPrice.ElementAt(0));
+            int len = sku.unitPrice.Count;
+            IDictionary<string, int> numberOfItems = new Dictionary<string, int>();
+
+            // Fetching the number of items for each SKU.
+            for (int i=0; i < len; i++)
+            {
+                Console.WriteLine("\nEnter the number of items of {0} ", sku.unitPrice.ElementAt(i).Key);
+                numberOfItems.Add(sku.unitPrice.ElementAt(i).Key, Convert.ToInt32(Console.ReadLine()));                
+            }
+
             Console.ReadKey();
         }
     }
