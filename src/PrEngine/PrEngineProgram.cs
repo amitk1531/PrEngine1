913ee7b0aecfda4d76401bc871a138b50d509e67
amitk1531 <amitk1531@gmail.com>
2020-09-12 10:11:00 +0530

    Created Object

--- src/PrEngine/PrEngineProgram.cs
+++ src/PrEngine/PrEngineProgram.cs
@@ -5,12 +5,26 @@ using System.Threading.Tasks;
 
 namespace PrEngine
 {
-    public class PrEngineProgram
+    public class Cart
     {
+        IDictionary<string, int> unitPrice = new Dictionary<string, int>();
+        public Cart()
+        {
+            unitPrice.Add("A", 50);
+            unitPrice.Add("B", 30);
+            unitPrice.Add("C", 20);
+            unitPrice.Add("D", 15);
+        }
         public static void Main(string[] args)
         {
             Console.WriteLine("Hello!! - Press any key to continue adding to your cart...");
             Console.ReadKey(true);
+
+            // Creating Object.
+            Cart sku = new Cart();
+
+            Console.WriteLine(sku.unitPrice.ElementAt(0));
+            Console.ReadKey();
         }
     }
 }
